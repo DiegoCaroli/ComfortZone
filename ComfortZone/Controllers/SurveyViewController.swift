dd6869ea35185d7cc42019c0f8877f96923f79bd
Diego Caroli <diegocaroli@icloud.com>
2017-12-12 17:19:33 +0100

    Create design for SurveyViewController and coding it.

--- ComfortZone/Controllers/SurveyViewController.swift
+++ ComfortZone/Controllers/SurveyViewController.swift
@@ -9,56 +9,65 @@
 import UIKit
 
 class SurveyViewController: UIViewController {
+  
+  @IBOutlet weak var haveDone: UIButton!
+  @IBOutlet weak var wantToDo: UIButton!
+  @IBOutlet weak var wouldNotDo: UIButton!
+  @IBOutlet weak var titleSurveyLabel: UILabel!
+  @IBOutlet weak var backgroundSurveyImageView: UIImageView!
+  
+  var counter = 0
+  var index = 0
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    customizeButton(haveDone)
+    customizeButton(wantToDo)
+    customizeButton(wouldNotDo)
     
-    @IBOutlet weak var haveDone: UIButton!
-    @IBOutlet weak var wantToDo: UIButton!
-    @IBOutlet weak var wouldNotDo: UIButton!
-    
-    @IBOutlet weak var titleSurveyLabel: UILabel!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        customizeButton(haveDone)
-        customizeButton(wantToDo)
-        customizeButton(wouldNotDo)
+    nextQuestion(index: index)
+  }
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+  @IBAction func haveDoneButtonPressed(_ sender: Any) {
+    nextQuestion(index: index)
+
+  }
+  
+  @IBAction func wantToDoButtonPressed(_ sender: Any) {
+    nextQuestion(index: index)
+  }
+  
+  @IBAction func wouldNotDoButtonPressed(_ sender: Any) {
+    nextQuestion(index: index)
+  }
+  
+  private func customizeButton(_ button: UIButton) {
+    button.layer.cornerRadius = 10
+//    button.layer.borderColor = UIColor.black.cgColor
+//    button.layer.borderWidth = 1.3
+    button.backgroundColor = UIColor.white
+    button.layer.shadowColor = UIColor.gray.cgColor
+    button.layer.shadowOffset = CGSize(width: 2, height: 4)
+    button.layer.shadowOpacity = 0.8
+  }
+  
+  private func nextQuestion(index: Int) {
+    if index < DataQuestion.shared.questions.count {
+      let question = DataQuestion.shared.questions[index]
+      titleSurveyLabel.text = question.text
+      backgroundSurveyImageView.image = question.backgroundImage
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
-    private func customizeButton(_ button: UIButton) {
-        button.layer.cornerRadius = 10
-        button.layer.borderColor = UIColor.black.cgColor
-        button.layer.borderWidth = 1.3
-        button.layer.shadowColor = UIColor.gray.cgColor
-        button.layer.shadowOffset = CGSize(width: 2, height: 4)
-        button.layer.shadowOpacity = 0.8
-    }
-    
-    
-    @IBAction func haveDoneButtonPressed(_ sender: Any) {
-        
-    }
-    
-    @IBAction func wantToDoButtonPressed(_ sender: Any) {
-        
-    }
-    
-    @IBAction func wouldNotDoButtonPressed(_ sender: Any) {
-        
-    }
-    
+    self.index += 1
+  }
+  
 }
---
+++ ComfortZone/Data Layer/DataQuestion.swift
@@ -0,0 +1,29 @@
+//
+//  DataQuestion.swift
+//  ComfortZone
+//
+//  Created by Diego Caroli on 12/12/2017.
+//  Copyright © 2017 Diego Caroli. All rights reserved.
+//
+
+import Foundation
+
+final class DataQuestion {
+  
+  static let shared = DataQuestion()
+  var questions: [Question]
+  
+  private init() {
+    questions = [Question(text: "done bungee jumping", backgroundImage: #imageLiteral(resourceName: "BungeeJumping")),
+                     Question(text: "done climbing mountain", backgroundImage: #imageLiteral(resourceName: "ClimbingMountain")),
+                     Question(text: "done paintballing", backgroundImage: #imageLiteral(resourceName: "Paintballing")),
+                     Question(text: "done public speaking", backgroundImage: #imageLiteral(resourceName: "PublicSpeakingView")),
+                     Question(text: "stared your own business", backgroundImage: #imageLiteral(resourceName: "OwnBusinesView")),
+                     Question(text: "worked in a different Country", backgroundImage: #imageLiteral(resourceName: "JobContryView")),
+                     Question(text: "travelled alone", backgroundImage: #imageLiteral(resourceName: "FirstStep")),
+                     Question(text: "quitted a job", backgroundImage: #imageLiteral(resourceName: "SecondStep")),
+                     Question(text: "became comfortable with dating", backgroundImage: #imageLiteral(resourceName: "ThirdStep")),
+    ]
+  }
+  
+}
