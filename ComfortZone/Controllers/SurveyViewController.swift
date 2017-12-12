b06571dfb15b0b76931415e2213a473a92031c90
MariapiaPansini <pansinimary95@gmail.com>
2017-12-12 11:54:32 +0100

    new version SurveyViewController

--- ComfortZone/Controllers/SurveyViewController.swift
+++ ComfortZone/Controllers/SurveyViewController.swift
@@ -14,9 +14,14 @@ class SurveyViewController: UIViewController {
     @IBOutlet weak var wantToDo: UIButton!
     @IBOutlet weak var wouldNotDo: UIButton!
     
+    @IBOutlet weak var titleSurveyLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         customizeButton(haveDone)
+        customizeButton(wantToDo)
+        customizeButton(wouldNotDo)
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,7 +42,23 @@ class SurveyViewController: UIViewController {
     private func customizeButton(_ button: UIButton) {
         button.layer.cornerRadius = 10
         button.layer.borderColor = UIColor.black.cgColor
-        button.layer.borderWidth = 2
+        button.layer.borderWidth = 1.3
+        button.layer.shadowColor = UIColor.gray.cgColor
+        button.layer.shadowOffset = CGSize(width: 2, height: 4)
+        button.layer.shadowOpacity = 0.8
+    }
+    
+    
+    @IBAction func haveDoneButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func wantToDoButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func wouldNotDoButtonPressed(_ sender: Any) {
+        
     }
     
 }
