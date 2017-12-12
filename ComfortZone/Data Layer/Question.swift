0247bebf9f18deb3f78ad62de3a051098db3a19c
Diego Caroli <diegocaroli@icloud.com>
2017-12-12 11:43:31 +0100

    Create Question model.

---
+++ ComfortZone/Data Layer/Question.swift
@@ -0,0 +1,17 @@
+//
+//  Question.swift
+//  ComfortZone
+//
+//  Created by Diego Caroli on 11/12/2017.
+//  Copyright © 2017 Diego Caroli. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Question {
+  
+  let text: String
+  let backgroundImage: UIImage
+
+}
