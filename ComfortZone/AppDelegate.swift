1db4d089aafe874fe96b1269018ef1c33480822c
Diego Caroli <diegocaroli@icloud.com>
2018-01-26 14:05:18 +0100

    fix.

--- ComfortZone/AppDelegate.swift
+++ ComfortZone/AppDelegate.swift
@@ -30,10 +30,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     return true
   }
-  
-  private func afterDelay(_ seconds: Double, closure: @escaping () -> ()) {
-    DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: closure)
-  }
 
   func applicationWillResignActive(_ application: UIApplication) {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
